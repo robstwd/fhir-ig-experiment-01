624acdd02e70c7c3b4e3d61c8be7e45f536140ca
robstwd <robstwd@users.noreply.github.com>
2020-09-04 02:38:05 +0000

    deploy: c3119e2270e49bb88ab863e620ec7cfde9924585

--- features/support/env.rb
+++
@@ -1,7 +0,0 @@
-$LOAD_PATH << File.expand_path('../../../lib',__FILE__)
-require 'nokogiri'
-require 'rspec'
-require 'rubygems'
-require 'date'
-
-Dir["#{File.expand_path('../../../spec/support/matchers/matchers', __FILE__)}*.rb"].each {|f| require f}
